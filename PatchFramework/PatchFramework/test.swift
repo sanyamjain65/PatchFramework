0b4ad3c764299d4614165511b3d7150068ed5083
sanyam <sanyam.j65@gmail.com>
2018-08-02 00:49:11 +0530

    learning frameworks in ios

---
+++ PatchFramework/PatchFramework/test.swift
@@ -0,0 +1,17 @@
+//
+//  test.swift
+//  PatchFramework
+//
+//  Created by Sanyam Jain on 01/08/18.
+//  Copyright © 2018 Sanyam Jain. All rights reserved.
+//
+
+import Foundation
+
+public class test {
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Did some stuff, here you go"
+    }
+}
